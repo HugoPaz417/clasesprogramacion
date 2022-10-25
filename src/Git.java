0b16158b8fa36a4728886a2224ea67bf12890bc3
hugopaz417 <netoblion2000@gmail.com>
2022-10-25 13:34:43 -0500

    first commit

--- src/Git.java
+++ src/Git.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Git {
+public class Git {
+    public static void main(String[] args){
+        System.out.println("Comandos git utilizados desde la inicialización del proyecto a  git hasta la colocación de los archivos en github");
+            System.out.println("git config --global luego se debe colocar el email");
+            System.out.println("git config user.mail user.mail aquí debe aparecer el email que colocamos anteriormente");
+            System.out.println("git config --global user.name colocamos nuestro nombre de github");
+            System.out.println("git config user.name y nos debe aparecer el nombre que colocamos");
+            System.out.println("git init");
+            System.out.println("git add .");
+            System.out.println("git branch -M main");
+            System.out.println("git remote add origin y colocamos el link de nuestro repositorio en github");
+            System.out.println("git push -u origin main");
+    }
 }
