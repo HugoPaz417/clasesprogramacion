a446b9021968593c3ce2b892629441c13cc0e849
hugopaz417 <netoblion2000@gmail.com>
2022-10-25 13:04:53 -0500

    first commit

---
+++ src/Contenido.java
@@ -0,0 +1,25 @@
+public class Contenido {
+ public static void main(String[] args){
+     System.out.println("Conceptos básicos de programación");
+            System.out.println("Algoritmo, es la secuencia de pasos lógicos que resuelven un problema");
+            System.out.println("Variable, es donde se almacenan y recuperan los datos de un programa");
+            System.out.println("Software de sistema, son las instrucciones que mandamos a la computadora y que permiten el manejo de esta, por ejemplo tenemos Windows, Linux, Mac");
+            System.out.println("Software de aplicación, lo es cualquier aplicación que tengamos instalada en la computadora, por ejemplo: Word, Google Chrome, Opera, Microsoft EDGE, Valorant");
+            System.out.println("Lenguajes de programación, son las expresiones que expresan un algoritmo, los lenguajes más usados son: java, c++, python");
+            System.out.println("Código binario, es el sistema numérico usado para la representación de textos, o procesados de instrucciones de computadora, este código usa solamente 1 y 0 para representarlos");
+            System.out.println("El bit, es el elementos más pequeño en una computadora, puede asumir el valor de 1 ó 0");
+            System.out.println("Byte, es el conjunto de 8 bits que representa la unidad mínima almacenable en un computador");
+            System.out.println("Múltiplos del byte, 1 KB = 1024 bytes, 1 MB = 1024 KB, 1 GB = 1024 MB, 1 TB = 1024 GB, 1 PB = 1024 TB, 1EB = 1024 PB, 1ZB = 1024 EB");
+            System.out.println("Entorno de programación, es uno o varios programas que ayudan al programador teniendo todas las tareas necesarias para el desarrollo de un programa o aplicación");
+            System.out.println("Cloud computing, se encarga de ofrecer servicios a través de la conectividad y gran escala de internet");
+            System.out.println("Bug, son los problemas o errores que tiene nuestro código al momento de ejecutarlo");
+            System.out.println("Boot, es el proceso de inicialización, en el que se verifica que las partes y configuraciones mínimas necesarias estén funcionando");
+            System.out.println("Console, es la ventana desde donde se trabaja en la línea de comandos");
+            System.out.println("Terminal, es una interfaz de línea de comandos, es decir, una interfaz que se usa para enviar comandos a la computadora");
+            System.out.println("Plugin, es una extensión de cógido que se puede agregar a un código en el que se está trabajando");
+            System.out.println("Compilador, transforma el código en un lenguaje de programación a un lenguaje de máquina que lee el procesador");
+            System.out.println("Código fuente, código de un programa, la computadora lo usa para generar una versión del programa que es capaz de procesar");
+            System.out.println("Shell, es una ventana que sirve para manipular el sistema operativo a través de la línea de comandos");
+            System.out.println("Project error, esto se da cuando en el código que programamos hay un error por falta de signos");
+ }
+}
